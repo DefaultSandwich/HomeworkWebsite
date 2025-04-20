e15b4dbc93600211007e64ba727d77fe8bdd4ec1
DefaultSandwich <aavuong2009@gmail.com>
2025-04-20 21:31:48 +1000

    fixed ticks not displaying

--- icons/icons.js
+++ icons/icons.js
@@ -1,5 +1,5 @@
-const tick = "<img src = '../Icons//tick.svg' style = 'height : 1.2em; width : 1.2em; vertical-align:baseline ; position: relative; top: calc((1.2em - 0.735em) / 2)'>"
-const cross = "<img src = '../Icons//cross.svg' style = 'height : 1.2em; width : 1.2em; vertical-align:baseline ; position: relative; top: calc((1.2em - 0.735em) / 2)'>"
+const tick = "<img src = '../icons//tick.svg' style = 'height : 1.2em; width : 1.2em; vertical-align:baseline ; position: relative; top: calc((1.2em - 0.735em) / 2)'>"
+const cross = "<img src = '../icons//cross.svg' style = 'height : 1.2em; width : 1.2em; vertical-align:baseline ; position: relative; top: calc((1.2em - 0.735em) / 2)'>"
 
 
 function formatValue(JSON) {
